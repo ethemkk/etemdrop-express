ddda81c0f5cd3fe2263a584c202bf9be763cf1e5
Güney Yılmazer <guneyfewm@gmail.com>
2024-09-10 03:19:23 +0300

    vercel

--- config/db.js
+++ config/db.js
@@ -1,16 +1,20 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config();
+let isConnected = false; // Track connection status
 
 const connectDB = async () => {
+  if (isConnected) {
+    return;
+  }
+
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    const db = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+    isConnected = db.connections[0].readyState;
     console.log("MongoDB connected");
   } catch (err) {
-    console.error(err.message);
+    console.error("MongoDB connection error:", err.message);
     process.exit(1);
   }
 };
